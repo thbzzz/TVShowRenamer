9d952521cb62bd6f7fb47e67a478638a2941c84f
0xthbz <sk8clem35@gmail.com>
2019-10-18 01:01:09 +0200

    Version 1.0

--- tv_show_renamer.py
+++ tv_show_renamer.py
@@ -8,23 +8,27 @@ from docopt import docopt
 usage = """TV Show Renamer
 
 Usage:
-  tv_show_renamer.py -d <dirname> -n <tv_show_name> -s <season> [--brave] [-v]
+  tv_show_renamer.py -d <dirname> -n <tv_show_name> -s <season> [--brave] [-q]
   tv_show_renamer.py (-h | --help)
 
 Options:
   --brave   Be brave and move instead of copying
-  -v        Verbose mode
+  -q        Quiet mode
+
+Example:
+  tv_show_renamer.py -d . -n 'Downloaded Legally' -s 1
 """
 
 class TVShow():
     """"""
-    def __init__(self, dirname: str, tvshowname: str, season: int, brave: bool, verbose: bool):
+    def __init__(self, dirname: str, tvshowname: str, season: int, brave: bool, quiet: bool):
         """"""
         self.dirname = str(dirname)
         self.tvshowname = str(tvshowname)
         self.season = int(season)
         self.extension = ""
         self.brave = bool(brave)
+        self.quiet = bool(quiet)
         self.run()
 
     def run(self):
@@ -35,19 +39,40 @@ class TVShow():
                 exit()
 
         files_old, files_new = self.gen()
+        files_old = self.sortList(files_old)
 
         if self.brave:
-            if self.warning():
-                self.moveFiles(files_old, files_new)
-            else:
-                print("[*] Cancelled")
+            self.moveFiles(files_old, files_new)
         else:
             self.copyFiles(files_old, files_new)
 
+        if not self.quiet:
+            print("[+] Done")
+
     def copyFiles(self, old, new):
         """"""
+        if not self.quiet:
+            print("[+] Copying files...")
+
         for old_new in zip(old, new):
             shutil.copyfile(old_new[0], old_new[1])
+
+    def moveFiles(self, old, new):
+        """"""
+        if not self.quiet:
+            print("[+] Moving files...")
+
+        for old_new in zip(old, new):
+            try:
+                shutil.move(old_new[0], old_new[1])
+            except SameFileError:
+                pass
+
+    def sortList(self, ep_list):
+        """"""
+        sorted_list = sorted(ep_list)
+        return sorted_list
+
 
     def format(self, index):
         """"""
@@ -69,14 +94,9 @@ class TVShow():
 
         return (files_src, files_dst)
 
-    def moveFiles(self, old, new):
-        """"""
-        for old_new in zip(old, new):
-            shutil.move(old_new[0], old_new[1])
-
     def warning(self):
         """"""
-        warning_msg = "[!] Warning: This will permanently rename ALL FILES in %s.\n\
+        warning_msg = "[!] Warning: This will permanently rename ALL FILES in '%s'.\n\
              Renaming is based on files' current order, this might MESS UP your files.\n" % (self.dirname)
 
         print(warning_msg)
@@ -86,12 +106,12 @@ class TVShow():
 
 
 if __name__ == "__main__":
-    args = docopt(usage , version="0.1")
+    args = docopt(usage , version="1.0")
 
     tvshow = TVShow(
         args["<dirname>"],
         args["<tv_show_name>"],
         args["<season>"],
         args["--brave"],
-        args["-v"]
+        args["-q"]
     )
